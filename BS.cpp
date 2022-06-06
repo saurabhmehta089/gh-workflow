713bc129465f85a5d7547fd8427b027a2a9fed94
Saurabh <57309205+saurabhmehta1601@users.noreply.github.com>
2022-06-06 18:21:22 +0530

    fix typo in BS

--- BS.cpp
+++ BS.cpp
@@ -5,8 +5,8 @@ using namespace std ;
 int main(){
   int arr[] = {123,35,56,43,22} ;
   int n = sizeof(arr)/sizeof(arr[0]) ;
-  for(int i =0 ; i < n - 1 - i ;i++){
-    for(int j = 0 ; j < n - 1; j++){
+  for(int i =0 ; i < n - 1  ;i++){
+    for(int j = 0 ; j < n - 1 - i; j++){
       if(arr[j] > arr[j+1] ){
         swap(arr[j], arr[j+1]) ;
       }
