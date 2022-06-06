4f6cdae5c10e825aee946f15fc8efcc9883d9731
saurabhmehta089 <76899912+saurabhmehta089@users.noreply.github.com>
2022-06-06 18:05:55 +0530

    create BS.cpp
    fixes #1

---
+++ BS.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+using namespace std ;
+
+int main(){
+  int arr[] = {123,35,56,43,22} ;
+  int n = sizeof(arr)/sizeof(arr[0]) ;
+  for(int i =0 ; i < n - 1;i++){
+    for(int j = 0 ; j < n - 1; j++){
+      if(arr[j] > arr[j+1] ){
+        swap(arr[j], arr[j+1]) ;
+      }
+    }
+  }
+ 
+return 0;
+}
